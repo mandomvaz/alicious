89e4af25fdc188eaf9590d5fecb39c6c2c55cd21
Manuel Domínguez Vázquez <mandomvaz@gmail.com>
2022-09-29 08:32:02 +0200

    .

--- backend/src/applications/AliciousAPI/UserAPI/UserMapper.cs
+++ backend/src/applications/AliciousAPI/UserAPI/UserMapper.cs
@@ -1,5 +1,7 @@
 ﻿using AliciousAPI.ViewModels;
 
+using Google.Apis.Auth;
+
 namespace AliciousAPI.UserAPI
 {
     public static class UserMapper
@@ -9,23 +11,42 @@ namespace AliciousAPI.UserAPI
             app.MapPost("/user/login", UserLogin);
         }
 
-        public static IResult UserLogin(UserLoginRequest googletoken)
+        public static IResult UserLogin(IUserService UserService, IIssueService IssueService,  UserLoginRequest googletoken)
         {
-            return ResponseViewModel.Create(true, new UserLoginResponse()
+            var GoogleTask = GoogleJsonWebSignature.ValidateAsync(googletoken.googletoken);
+
+            GoogleTask.Wait();
+
+            if (GoogleTask.IsCompletedSuccessfully)
             {
-                user = new UserViewModel()
+                UserDTO user = UserService.RetrieveByEmail(GoogleTask.Result.Email);
+
+                Guid rootGuid = IssueService.RetrieveRootGuid(user.Guid);
+
+                return ResponseViewModel.Create(true, new UserLoginResponse()
                 {
-                    uid = "fasdfasdf",
-                    name = "Lolo",
-                    email = "asdfas",
-                    rootiid = "",
-                    sub = "asdfasdf",
-                    fullname = "asdfasdf",
-                    pictureurl = "asdfasdf",
-                    created = "asdfasdfa",
-                },
-                token = "token",
-            });
+                    user = new UserViewModel()
+                    {
+                        uid = user.Guid.ToString(),
+                        name = user.Name,
+                        email = user.Email,
+                        rootiid = rootGuid.ToString(),
+                        sub = user.Sub,
+                        fullname = user.FullName,
+                        pictureurl = user.Picture,
+                        created = user.Timestamp.ToShortDateString(),
+                    },
+                    token = googletoken.googletoken,
+                });
+            }
+            else
+            {
+                return Results.StatusCode(500);
+            }
+
+
+
+
         }
 
         public record UserLoginRequest
